d121c23452cdada8b166d92e40ca33951c87561a
maitabe <martushb@gmail.com>
2016-11-10 15:26:49 +0200

    added the comments box and updating the object

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ var posts = [];
 //update the post from the array
 function updatePosts() {
 	//empty the
-	$('.posts p').remove();
+	$('.posts').empty();
 	//print the data from the main array
 	for (var i = 0; i < posts.length; i++) {
 		var textPost = posts[i].name;
 		var idNum = posts[i].id;
 
-		$('.posts').append('<p class="post" data-id="'+idNum+'">' + textPost +'<a href="#" class="remove">'+ 'remove' +'</a> </p>');
-		removePost();
+		$('.posts').append(
+			 '<div class="post-wrap" data-id="' + idNum + '"><p class="post">' + textPost +'<a href="#" class="remove">'+ 'remove' +'</a> </p>'
+			+'<div class="well"><div class="form-group"><input type="text" class="username-input form-control" placeholder="Username"></input>'
+        	+'</div><div class="form-group"><input type="text" class="comment-input form-control" placeholder="Comment"></input>'
+        	+'</div><button type="button" class="btn btn-primary add-comment">Comment</button></div></div>'
+        );
+
+		// removePost();
 	}
 
 
@@ -24,12 +30,13 @@ function addPost(str) {
 	//new obj
 	var newPost = {
 		name:str,
-		id:undefined
+		id:undefined,
+		comments: []
 	};
 
 	//if id is undefined, add a number
 	if(posts.length === 0) {
-		newPost.id = 1;
+		newPost.id = 0;
 
 	}else{
 		newPost.id = posts[posts.length-1].id + 1;
@@ -45,30 +52,33 @@ function addPost(str) {
 
 }
 
-// remove one post
-function removePost() {
+//add comment and username to the comment array inside posts obj
+function addComment(username, comment, id){
 
-	$('a.remove').closest('p').on('click', function(){
+	var newComment = {
+	 	username: username,
+	 	comment: comment,
+	 };
 
-		//loop through the post array to delete post selected
-		for (var i = 0; i < posts.length; i++) {
-			//grab the data-id from element p
-			var pItem = $(this).closest('p').data().id;
-			// var indexPost = posts[i].id;
-			if(pItem === posts[i].id) {
-				posts.splice(i, 1);
-				$(this).closest('p').remove();
-				return;
-			}
+console.log(posts);
+	for (var i = 0; i < posts.length; i++) {
+		if(id===posts[i].id) {
+			posts[i].comments.push(newComment);
 		}
+	}
+	// var newComment =  new Comment(username, comment);
 
-	})
+
+	// posts[comments].push(newComment);
 }
 
+// remove one post
+function removePost(id) {
 
+	posts.splice(id, 1);
 
+}
 
-$(document).ready(function(){
 
 	//event handlers
 
@@ -83,6 +93,29 @@ $(document).ready(function(){
 
 	});
 
+	//remove post
+	$('.posts').on('click','.remove', function(){
+		//remove from array
+		var id = $(this).closest('.post-wrap').data().id;
+		removePost(id);
+		//remove from DOM
+		$(this).closest('.post-wrap').remove();
 
-});
+	});
+
+//ADD COMMENT CLICK
+$('.posts').on('click', ".add-comment",function() {
+		var id = $(this).closest('.post-wrap').data().id;
+
+		alert(id);
+		var username = $(this).prev().prev().find('input').val();
+		var comment = $(this).prev().find('input').val();
+
+		console.log(username);
+		console.log(comment);
+
+		addComment(username, comment, id);
+
+	});
+
 
